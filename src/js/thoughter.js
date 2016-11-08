6e82f9497e4924f49bb289d363c2d34a9fcf6b44
Ettore Merendino <merendino.ettore@gmail.com>
2016-11-08 16:49:42 -0500

    I linked my js files to my src html and started my js coding

--- src/js/thoughter.js
+++ src/js/thoughter.js
@@ -1,29 +1,12 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    window.hello = window.hello || {};
+  window.ns = window.ns || {};
 
-    window.hello.sayHello = sayHello;
-    window.hello.sayGoodbye = sayGoodbye;
+  $(window)
+      .on('hashchange', function (eventObj) {
 
+        console.log('hash changed', window.location);
 
-    /**
-     * This function returns a greeting if you give
-     * it a name, otherwise it might yell at you.
-     * @param  {String} name The name to say hello to
-     * @return {String}      The greeting for you
-     */
-    function sayHello(name) {
-        if (name === undefined) {
-            return 'Where did you go?';
-        } else if (typeof(name) !== 'string') {
-            return 'You are a liar.';
-        }
-        return 'Hello ' + name;
-    }
-
-    function sayGoodbye(name) {
-        return 'Goodbye ' + name;
-    }
-
-})();
+      } )
+}());
