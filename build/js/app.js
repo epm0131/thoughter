d686e87bc144fd927ef0ac8da0e46c6b28a6bd5d
Ettore Merendino <merendino.ettore@gmail.com>
2016-11-10 16:53:35 -0500

    I corrected my connect grunt

---
+++ build/js/app.js
@@ -0,0 +1,59 @@
+(function() {
+  'use strict';
+
+  window.thought = window.thought || {};
+
+  window.thought.searchThoughts = searchThoughts;
+  /**
+   * Use this function to pull thoughts from the API
+   * @return {Promise}  the ajax call promise.
+   */
+  function searchThoughts() {
+    return $.ajax({
+      url: 'https://thoughter.herokuapp.com/api/Thoughts',
+      method: 'GET',
+      dataType: 'json',
+      data: { foo: 'bar '} //example - goes into the body
+    });
+
+ }
+
+
+
+
+
+
+}());
+
+(function() {
+  'use strict';
+
+  window.thought = window.thought || {};
+
+  $(window)
+      .on('hashchange', function() {
+        $('main').hide();
+        var currentHash = window.location.hash;
+        $(currentHash).show();
+      });
+}());
+
+(function() {
+  'use strict';
+
+window.thought = window.thought || {};
+
+window.thought.addThoughtsToPage = addThoughtsToPage;
+
+function addThoughtsToPage(thoughts) {
+  thoughts.forEach(function addLi(thought) {
+    $('ul.list').append('<li>' + thought.id + '</li>');
+  });
+}
+
+
+
+
+
+
+}());
