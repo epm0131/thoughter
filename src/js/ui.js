67e638a69415b7fc6ffafd4ceb02d2ba8256cd49
Ettore Merendino <merendino.ettore@gmail.com>
2016-11-09 22:15:48 -0500

    I forgot to add a file

---
+++ src/js/ui.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+
+window.thought = window.thought || {};
+
+window.thought.addThoughtsToPage = addThoughtsToPage;
+
+function addThoughtsToPage(thoughts) {
+  thoughts.forEach(function addLi(thought) {
+    $('ul.list').append('<li>' + thought.id + '</li>');
+  });
+}
+
+
+
+
+
+
+}());
