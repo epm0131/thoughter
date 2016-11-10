7c246da1b1ba55e182e0a7f3f415871572ba849a
Ettore Merendino <merendino.ettore@gmail.com>
2016-11-09 22:15:22 -0500

    I am trying to get a DOM test to work

--- src/js/data.js
+++ src/js/data.js
@@ -4,7 +4,11 @@
   window.thought = window.thought || {};
 
   window.thought.searchThoughts = searchThoughts;
-
+  /**
+   * Use this function to pull thoughts from the API
+   * @param {Array}
+   * @return {[type]} [description] 
+   */
   function searchThoughts() {
     return $.ajax({
       url: 'https://thoughter.herokuapp.com/api/Thoughts',
--- test/specs/data.spec.js
+++ test/specs/data.spec.js
@@ -11,6 +11,8 @@
       it('should make an ajax call and grab a thought', function(doneCallback) {
         var xhrPromise = window.thought.searchThoughts();
         expect(xhrPromise).to.be.an('object');
+        expect(xhrPromise.done).to.be.a('function');
+        expect(xhrPromise.fail).to.be.a('function');
 
         xhrPromise
           .done(function handleSuccess(data) {
@@ -21,9 +23,20 @@
             console.log(xhrPromise);
             doneCallback();
           })
-
    });//it make an ajax call
    });//describe thoughts
+
+   describe('adds thoughts to a page', function() {
+     it('should add thoughts to the page when given an array', function(){
+       window.thought.addThoughtsToPage([
+         {id: 1},
+         {id: 2},
+       ]);
+
+       var thoughtLi =$('ul.list li')
+       expect(thought.length).to.equal(1);
+     });
+   })
  });//it namespace
  });//describe (main suite)
 }()); //iife
