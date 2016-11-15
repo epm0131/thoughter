07e97b3d863103e14443b2ac650f0b23d818fedd
Ettore Merendino <merendino.ettore@gmail.com>
2016-11-15 09:01:48 -0500

    I did some refactoring

---
+++ src/js/thought-add.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+
+window.thought = window.thought || {};
+
+$('.newThought')
+    .on('submit', function submitNewThought(event) {
+        event.preventDefault();
+        $('.btn btn-primary').trigger('reset');
+        window.thought.addNewThought($('.form-control').val());
+        console.log('hi');
+    });
+
+
+
+
+
+
+}());
